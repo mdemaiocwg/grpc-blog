602fb506748f09ed083d2b8f59a677cb06aae68f
Matthew DeMaio <MDemaio@NY.CORP.INT>
2020-08-12 13:24:21 -0400

    Updated styling of frontend

--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -13,18 +13,16 @@
           <button @click="AddBlog" class="btn btn-primary">Add Blog</button>
         </div>
     </section>
-    <section>
-      <div class="row">
-        <div class="offset-md-4 col-md-4">
-          <ul class="list-group justify-content-center">
+    <section class="mt-5">
+      <div class="row mb-5" v-for="i in Math.ceil(blogs.length / 3)">
+          <ul class="list-group list-group-horizontal mr-5">
             <li
-              class="row list-group-item border mt-2 col-xs-1"
-              v-for="blog in blogs" BELONGS ABOVE IN LI
+              class="row list-group-item border ml-5"
+              v-for="blog in blogs.slice((i-1) * 3, i * 3)"
               v-bind:key="blog._id"
             >
               <div class="row justify-content-center">
                 <h1>{{blog.Title}}</h1> 
-                 <!-- <span @click="DeleteBlog(blog)" class="offset-sm-1 col-sm-2 delete text-right">X</span> -->
               </div>
               <div class="row justify-content-center">
                 <span class="author">{{blog.AuthorId}} • </span>
@@ -34,18 +32,22 @@
                 <p>{{blog.Content}}
                 </p>
               </div>
-              <div class="row justify-content-center">
+              <div class="row justify-content-center mt-4">
                 <button @click="DeleteBlog(blog)" class="btn btn-danger">Delete Blog</button>
               </div>
             </li>
           </ul>
-        </div>
       </div>
     </section>
+   <footer class="footer">
+    <a href="https://www.github.com/mdemaio/" class="footer-item" target="_blank">Github</a>
+    <a href="https://www.linkedin.com/in/matthew-demaio-6848ab15a/" class="footer-item" target="_blank">Linkedin</a>
+  </footer>
   </div>
 </template>
 
 <script>
+const isDev = true;
 import { ListBlogRequest, ReadBlogResponse, CreateBlogRequest, CreateBlogResponse, DeleteBlogRequest, Blog } from "../blogpb/blog_pb";
 import { BlogServiceClient } from "../blogpb/blog_grpc_web_pb";
 import "bootstrap/dist/css/bootstrap.css";
@@ -53,7 +55,8 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 
 export default {
   name: "app",
-  components: {},
+  components: {
+  },
   data: function() {
     return {
       inputTitle: "",
@@ -64,11 +67,11 @@ export default {
   },
   created: function() {
     this.client = new BlogServiceClient("http://localhost:8080", null, null);
-    //this.AddBlog();
     this.ListBlog();
   },
   methods: {
-    ListBlog: function(id) {
+    ListBlog: function() {
+      if (!isDev){
       let getRequest = new ListBlogRequest();
       let stream = this.client.listBlog(getRequest, {});
       this.blogs = [];
@@ -85,6 +88,12 @@ export default {
       stream.on('end', function(end) {
         console.log("END OF STREAM!");
       });
+      } else{
+        for(var i = 0; i < 12; i++){
+          const blog = {_Id: "TESTID", Title: "Matt Test Title", Content: "Matt testing content 123 123 123 Test 1234", AuthorId: "Matthew DeMaio", CreatedTime: "8/12/2020, 12:09:01 PM"};
+          this.blogs.push(blog);
+        }
+      }
     },
     AddBlog: function() {
       let request = new CreateBlogRequest();
@@ -92,8 +101,6 @@ export default {
       blog.setAuthorId(this.inputAuthor);
       blog.setTitle(this.inputTitle);
       blog.setContent(this.inputContent);
-      //const date = new proto.google.protobuf.Timestamp()
-      //blog.setCreatedTime(date.fromDate(new Date()));
 
       request.setBlog(blog);
 
@@ -121,7 +128,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+   min-height:100%;
+   position:relative;
 }
 
 .title-text {
@@ -135,5 +144,45 @@ export default {
 
 .author{
   font-style: italic;
+}
+
+.footer-item{
+  color: black;
+  font-size: 18px;
+  font-weight:500;
+  margin: 25px !important;
+}
+
+.footer {
+ position: fixed;
+ bottom: 0px;
+ width: 100%;
+ padding: 2px;
+}
+
+footer {
+  background: #007bff;
+  color: white;
+  
+}
+
+ul > li{
+  width: 30%;
+  list-style: none;
+  display: block;
+  float: left;
+}
+
+ul{
+  width: 100%;
+}
+
+body{
+  margin: 0px;
+}
+
+html, 
+body {
+    height: 100%;
 }
 </style>
